149b99f296c4e35daa7a06d93b765a9b5aa6f386
tkovacs-2 <tamas.2.kovacs@nokia.com>
2026-08-15 22:07:16 +0800

    Change find dialog dpi together with the frame

--- windows/finddlg.c
+++ windows/finddlg.c
@@ -1,30 +1,126 @@
 #include "finddlg.h"
-#include "finddlg_res.h"
-#include "anchor.h"
+#include <assert.h>
+
+#define IDC_FINDDLG_EDIT 1001
+#define IDC_FINDDLG_UP 1002
+#define IDC_FINDDLG_DOWN 1003
+#define IDC_FINDDLG_CLOSE 1004
+#define IDC_FINDDLG_GRIP 1005
+#define IDC_FINDDLG_IGNORE_CASE 1006
+#define IDC_FINDDLG_WHOLE_WORD 1007
 
 extern HINSTANCE hinst;
 extern HWND frame_hwnd;
-void init_window_dpi_info(HWND hwnd, POINT *dpi_info);
+extern POINT dpi_info;
 
-static POINT dialog_dpi_info;
+static const WCHAR dlg_class_name[] = L"FindDialog";
+
 static HWND finddlg_hwnd = NULL;
-static bool in_sizemove = false;
-static int zoomed_size = 0;
-static int normal_size = 0;
-static int initial_size = 0;
-static int zoomed_size_dpi = 0;
-static int normal_size_dpi = 0;
-static int initial_size_dpi = 0;
-static bool wm_size_pin = false;
 static int frame_top_offset = 0;
 static bool disable_notification = false;
+static HFONT finddlg_hfont = NULL;
+static HWND last_focus = NULL;
 
-static AnchorInfo anchor_info[] = {
-    {IDC_FINDDLG_EDIT, ANCHOR_TOP_RIGHT, OP_SIZE},
-    {IDC_FINDDLG_UP, ANCHOR_TOP_RIGHT, OP_MOVE},
-    {IDC_FINDDLG_DOWN, ANCHOR_TOP_RIGHT, OP_MOVE}
+typedef struct DlgRect {
+    int x;
+    int y;
+    int width;
+    int height;
+} DlgRect;
+
+typedef struct DlgControl {
+    int id;
+    const WCHAR *class_name;
+    const WCHAR *text;
+    DWORD style;
+    DWORD exstyle;
+    DlgRect rect;
+} DlgControl;
+
+static const DlgControl dlg_controls[] = {
+    {
+        IDC_FINDDLG_EDIT, L"EDIT", L"",
+        WS_CHILD | WS_VISIBLE | WS_TABSTOP | ES_AUTOHSCROLL,
+        WS_EX_CLIENTEDGE, {24, 1, 118, 14}
+    },
+    {
+        IDC_FINDDLG_UP, L"BUTTON", L"U",
+        WS_CHILD | WS_VISIBLE | WS_TABSTOP | BS_PUSHBUTTON,
+        0, {146, 1, 18, 14}
+    },
+    {
+        IDC_FINDDLG_DOWN, L"BUTTON", L"D",
+        WS_CHILD | WS_VISIBLE | WS_TABSTOP | BS_PUSHBUTTON,
+        0, {164, 1, 18, 14}
+    },
+    {
+        IDC_FINDDLG_CLOSE, L"BUTTON", L"x",
+        WS_CHILD | WS_VISIBLE | WS_TABSTOP | BS_PUSHBUTTON,
+        0, {4, 1, 16, 14}
+    },
+    {
+        IDC_FINDDLG_IGNORE_CASE, L"BUTTON", L"Ignore case",
+        WS_CHILD | WS_VISIBLE | WS_TABSTOP | BS_AUTOCHECKBOX,
+        0, {24, 17, 60, 10}
+    },
+    {
+        IDC_FINDDLG_WHOLE_WORD, L"BUTTON", L"Whole word",
+        WS_CHILD | WS_VISIBLE | WS_TABSTOP | BS_AUTOCHECKBOX,
+        0, {86, 17, 60, 10}
+    },
 };
-const int anchor_info_size = sizeof(anchor_info) / sizeof(anchor_info[0]);
+
+static const int dlg_control_count = sizeof(dlg_controls) / sizeof(dlg_controls[0]);
+static const DlgRect dlg_rect = {0, 0, 186, 28};
+static const int dlg_font_size = 8;
+static const WCHAR *dlg_font_name = L"MS Shell Dlg";
+
+static void make_font(void)
+{
+    assert(finddlg_hfont == NULL);
+    finddlg_hfont = CreateFontW(-MulDiv(dlg_font_size, dpi_info.y, 72),
+        0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET,
+        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+        DEFAULT_PITCH | FF_DONTCARE, dlg_font_name);
+}
+
+static void get_base_units(SIZE *base_units)
+{
+    HDC hdc = GetDC(frame_hwnd);
+    HFONT old_font = SelectObject(hdc, finddlg_hfont);
+    SIZE text_size;
+
+    GetTextExtentPoint32W(
+        hdc, L"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+        52, &text_size);
+
+    base_units->cx = (text_size.cx / 26 + 1) / 2;
+    base_units->cy = text_size.cy;
+
+    SelectObject(hdc, old_font);
+    ReleaseDC(frame_hwnd, hdc);
+}
+
+static int dlu_scale_x(int dlu, const SIZE *base_units)
+{
+    return MulDiv(dlu, base_units->cx, 4);
+}
+
+static int dlu_scale_y(int dlu, const SIZE *base_units)
+{
+    return MulDiv(dlu, base_units->cy, 8);
+}
+
+static DlgRect dlu_scale_rect(const DlgRect *dlu_rect, const SIZE *base_units)
+{
+    DlgRect pixel_rect;
+
+    pixel_rect.x = dlu_scale_x(dlu_rect->x, base_units);
+    pixel_rect.y = dlu_scale_y(dlu_rect->y, base_units);
+    pixel_rect.width = dlu_scale_x(dlu_rect->width, base_units);
+    pixel_rect.height = dlu_scale_y(dlu_rect->height, base_units);
+    return pixel_rect;
+}
 
 static void pin_to_frame(HWND hwnd)
 {
@@ -49,121 +145,13 @@ static void pin_to_frame(HWND hwnd)
     SetWindowPos(hwnd, NULL, dlg_left, pin_to.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_NOACTIVATE);
 }
 
-static void size_to_frame(HWND hwnd)
-{
-    int size = (IsZoomed(frame_hwnd) && zoomed_size_dpi > 0) ? zoomed_size_dpi : normal_size_dpi;
-    RECT r;
-    GetClientRect(frame_hwnd, &r);
-    int frame_size = r.right - r.left;
-    if (size > frame_size) {
-        size = frame_size;
-    }
-    GetWindowRect(hwnd, &r);
-    wm_size_pin = false;
-    SetWindowPos(hwnd, NULL, 0, 0, size, r.bottom - r.top, SWP_NOMOVE | SWP_NOZORDER | SWP_NOACTIVATE);
-    if (wm_size_pin) {
-        wm_size_pin = false;
-    } else {
-        pin_to_frame(hwnd);
-    }
-}
-
-static int dpi_scale(int x)
-{
-    return MulDiv(x, dialog_dpi_info.x, 96);
-};
-
-static int dpi_inverse_scale(int x)
-{
-    return MulDiv(x, 96, dialog_dpi_info.x);
-};
-
-static void store_initial_size(HWND hwnd)
-{
-    RECT r;
-    GetWindowRect(hwnd, &r);
-    initial_size_dpi = r.right - r.left;
-    initial_size = dpi_inverse_scale(initial_size_dpi);
-    if (normal_size > 0) {
-        normal_size_dpi = dpi_scale(normal_size);
-    } else {
-        normal_size_dpi = initial_size_dpi;
-        normal_size = initial_size;
-    }
-    if (zoomed_size > 0) {
-        zoomed_size_dpi = dpi_scale(zoomed_size);
-    }
-}
-
-static void store_size(HWND hwnd) {
-    RECT r;
-    GetWindowRect(hwnd, &r);
-    int size_dpi = r.right - r.left;
-    int size = dpi_inverse_scale(size_dpi);
-    if (IsZoomed(frame_hwnd)) {
-        zoomed_size_dpi = size_dpi;
-        zoomed_size = size;
-    } else {
-        normal_size_dpi = size_dpi;
-        normal_size = size;
-    }
-}
-
 static void apply_alpha(HWND hwnd, bool active)
 {
     BYTE alpha = active ? (BYTE)255 : (BYTE)128;
     SetLayeredWindowAttributes(hwnd, 0, alpha, LWA_ALPHA);
 }
 
-static void draw_grip(HDC hdc, const RECT *rcItem)
-{
-    int dot = dpi_scale(2);
-    int pitch = 2*dot;
-    int shift = dpi_scale(1);
-    int marginx = shift;
-    int marginy = ((rcItem->bottom - rcItem->top)%pitch)/2;
-    HBRUSH face = GetSysColorBrush(COLOR_3DFACE);
-    HBRUSH white = (HBRUSH)GetStockObject(WHITE_BRUSH);
-    HBRUSH shadow = GetSysColorBrush(COLOR_3DSHADOW);
-
-    FillRect(hdc, rcItem, face);
-    int x0 = rcItem->left + marginx;
-    int y0 = rcItem->top + marginy;
-
-    RECT wr, sr;
-
-    sr.left = x0;
-    sr.top = y0;
-    sr.right = sr.left + dot;
-    sr.bottom = sr.top + dot;
-
-    wr.left = x0 + shift;
-    wr.top = y0 + shift;
-    wr.right = wr.left + dot;
-    wr.bottom = wr.top + dot;
-
-    while (wr.bottom <= rcItem->bottom) {
-
-        while (wr.right <= rcItem->right) {
-            FillRect(hdc, &wr, white);
-            FillRect(hdc, &sr, shadow);
-            wr.left  += pitch;
-            wr.right += pitch;
-            sr.left += pitch;
-            sr.right += pitch;
-        }
-        wr.top  += pitch;
-        wr.bottom += pitch;
-        sr.top += pitch;
-        sr.bottom += pitch;
-        wr.left = x0 + shift;
-        wr.right = wr.left + dot;
-        sr.left = x0;
-        sr.right = sr.left + dot;
-    }
-}
-
-void notify_frame(HWND hwnd, UINT code)
+static void notify_frame(HWND hwnd, UINT code)
 {
     if (disable_notification) {
         return;
@@ -179,71 +167,18 @@ void notify_frame(HWND hwnd, UINT code)
 #define WM_DPICHANGED 0x02E0
 #endif
 
-static INT_PTR CALLBACK finddlg_proc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+static LRESULT CALLBACK finddlg_proc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg) {
-    case WM_INITDIALOG: {
-        SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED);
-        apply_alpha(hwnd, GetForegroundWindow() == hwnd);
-        dialog_dpi_info.x = 0;
-        init_window_dpi_info(hwnd, &dialog_dpi_info);
-        anchor_init(hwnd, &dialog_dpi_info, anchor_info, anchor_info_size);
-        store_initial_size(hwnd);
-        size_to_frame(hwnd);
-        return FALSE;
-    }
-    case WM_GETMINMAXINFO: {
-        MINMAXINFO *mmi = (MINMAXINFO *)lParam;
-        mmi->ptMinTrackSize.x = initial_size_dpi/2;
-        return TRUE;
-    }
-    case WM_NCHITTEST: {
-        LRESULT hit = DefWindowProc(hwnd, msg, wParam, lParam);
-        switch (hit) {
-        case HTTOPLEFT:
-        case HTBOTTOMLEFT:
-            hit = HTLEFT;
-            break;
-        case HTRIGHT:
-        case HTTOP:
-        case HTBOTTOM:
-        case HTTOPRIGHT:
-        case HTBOTTOMRIGHT:
-            hit = HTCLIENT;
-            break;
-        default:
-            break;
-        }
-        SetWindowLongPtr(hwnd, DWLP_MSGRESULT, (LONG_PTR)hit);
-        return TRUE;
-    }
-    case WM_ENTERSIZEMOVE:
-        in_sizemove = true;
-        return FALSE;
-    case WM_EXITSIZEMOVE:
-        in_sizemove = false;
-        store_size(hwnd);
-        pin_to_frame(hwnd);
-        return FALSE;
-    case WM_SIZE:
-        anchor_apply(hwnd, anchor_info, anchor_info_size);
-        if (in_sizemove) {
-            return FALSE;
-        }
-        wm_size_pin = true;
-        pin_to_frame(hwnd);
-        return FALSE;
     case WM_ACTIVATE:
         apply_alpha(hwnd, LOWORD(wParam) != WA_INACTIVE);
-        return FALSE;
-    case WM_DRAWITEM: {
-        LPDRAWITEMSTRUCT dis = (LPDRAWITEMSTRUCT)lParam;
-        if (dis->CtlType == ODT_STATIC && dis->CtlID == IDC_FINDDLG_GRIP) {
-            draw_grip(dis->hDC, &dis->rcItem);
-            return TRUE;
+        if (LOWORD(wParam) == WA_INACTIVE) {
+            HWND focus = GetFocus();
+            if (focus && IsChild(hwnd, focus)) {
+                last_focus = focus;
+            }
         }
         return FALSE;
-    }
     case WM_COMMAND:
         switch (LOWORD(wParam)) {
         case IDC_FINDDLG_EDIT:
@@ -285,25 +220,71 @@ static INT_PTR CALLBACK finddlg_proc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM 
         return FALSE;
     case WM_NCDESTROY:
         finddlg_hwnd = NULL;
-        return FALSE;
+        last_focus = NULL;
+        DeleteObject(finddlg_hfont);
+        finddlg_hfont = NULL;
+        return 0;
     case WM_DPICHANGED:
-        dialog_dpi_info.x = LOWORD(wParam);
-        dialog_dpi_info.y = HIWORD(wParam);
-        anchor_change_dpi(&dialog_dpi_info, anchor_info, anchor_info_size);
-        anchor_apply(hwnd, anchor_info, anchor_info_size);
-        zoomed_size_dpi = dpi_scale(zoomed_size);
-        normal_size_dpi = dpi_scale(normal_size);
-        initial_size_dpi = dpi_scale(initial_size);
-        return FALSE;
-    default:
+        return 0;
+    case WM_SETFOCUS:
+        if (!last_focus) {
+            last_focus = GetDlgItem(hwnd, IDC_FINDDLG_EDIT);
+        }
+        SetFocus(last_focus);
         return FALSE;
     }
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
+}
+
+static void register_finddlg_class()
+{
+    static bool registered = false;
+
+    if (registered) {
+        return;
+    }
+
+    WNDCLASSW wc;
+    memset(&wc, 0, sizeof(wc));
+    wc.lpfnWndProc = finddlg_proc;
+    wc.hInstance = hinst;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_3DFACE + 1);
+    wc.lpszClassName = dlg_class_name;
+
+    RegisterClassW(&wc);
+    registered = true;
+}
+
+static HWND create_dialog() {
+    register_finddlg_class();
+
+    make_font();
+
+    SIZE base_units;
+    get_base_units(&base_units);
+
+    DlgRect dr = dlu_scale_rect(&dlg_rect, &base_units);
+    HWND hwnd = CreateWindowExW(WS_EX_LAYERED, dlg_class_name, L"", WS_POPUP,
+        dr.x, dr.y, dr.width, dr.height, frame_hwnd, NULL, hinst, NULL);
+    apply_alpha(hwnd, GetForegroundWindow() == hwnd);
+    for (int i = 0; i < dlg_control_count; i++) {
+        const DlgControl *control = &dlg_controls[i];
+        dr = dlu_scale_rect(&control->rect, &base_units);
+        HWND child_hwnd = CreateWindowExW(
+            control->exstyle, control->class_name, control->text,
+            control->style, dr.x, dr.y, dr.width, dr.height,
+            hwnd, (HMENU)(INT_PTR)control->id, hinst, NULL);
+        SendMessageW(child_hwnd, WM_SETFONT, (WPARAM)finddlg_hfont, TRUE);
+    }
+    pin_to_frame(hwnd);
+    return hwnd;
 }
 
 void finddlg_create(WCHAR *pattern, bool activate, bool ignore_case, bool whole_word)
 {
     if (finddlg_hwnd == NULL) {
-        finddlg_hwnd = CreateDialog(hinst, MAKEINTRESOURCE(IDD_FINDDLG), frame_hwnd, finddlg_proc);
+        finddlg_hwnd = create_dialog();
     }
     disable_notification = true;
     SetWindowTextW(GetDlgItem(finddlg_hwnd, IDC_FINDDLG_EDIT), pattern);
@@ -312,6 +293,7 @@ void finddlg_create(WCHAR *pattern, bool activate, bool ignore_case, bool whole_
     CheckDlgButton(finddlg_hwnd, IDC_FINDDLG_WHOLE_WORD,
                    whole_word ? BST_CHECKED : BST_UNCHECKED);
     disable_notification = false;
+
     if (IsWindowVisible(finddlg_hwnd)) {
         if (activate) {
             SetActiveWindow(finddlg_hwnd);
@@ -338,15 +320,6 @@ void finddlg_pin_to_frame(int top_offset)
         return;
     }
     pin_to_frame(finddlg_hwnd);
-}
-
-void finddlg_size_to_frame(int top_offset)
-{
-    frame_top_offset = top_offset;
-    if (finddlg_hwnd == NULL) {
-        return;
-    }
-    size_to_frame(finddlg_hwnd);
 }
 
 int finddlg_get_text(WCHAR *buffer, int buffer_chars)
@@ -380,4 +353,35 @@ bool finddlg_get_whole_word()
 bool finddlg_is_dialog_message(MSG *msg)
 {
     return (finddlg_hwnd && IsDialogMessageW(finddlg_hwnd, msg));
+}
+
+void finddlg_relayout_to_dpi()
+{
+    if (finddlg_hwnd == NULL) {
+        return;
+    }
+    HWND hwnd = finddlg_hwnd;
+    HFONT old_font = finddlg_hfont;
+    finddlg_hfont = NULL;
+    make_font();
+
+    SIZE base_units;
+    get_base_units(&base_units);
+
+    DlgRect dr = dlu_scale_rect(&dlg_rect, &base_units);
+    SetWindowPos(hwnd, NULL, 0, 0, dr.width, dr.height, SWP_NOMOVE | SWP_NOZORDER | SWP_NOACTIVATE);
+
+    HDWP hdwp = BeginDeferWindowPos(dlg_control_count);
+    for (int i = 0; i < dlg_control_count; i++) {
+        const DlgControl *control = &dlg_controls[i];
+        HWND child_hwnd = GetDlgItem(hwnd, control->id);
+        SendMessageW(child_hwnd, WM_SETFONT, (WPARAM)finddlg_hfont, FALSE);
+        dr = dlu_scale_rect(&control->rect, &base_units);
+        hdwp = DeferWindowPos(
+            hdwp, child_hwnd, NULL, dr.x, dr.y, dr.width, dr.height,
+            SWP_NOZORDER | SWP_NOACTIVATE | SWP_NOCOPYBITS);
+    }
+    EndDeferWindowPos(hdwp);
+    InvalidateRect(hwnd, NULL, TRUE);
+    DeleteObject(old_font);
 }
